49fccc39f7b8d39448089302c62fe285a358e195
Crauzer <filip.quitko@gmail.com>
2019-02-09 14:53:59 +0100

    reads end data

--- Fantome.League/IO/RMAN/RMANFile.cs
+++ Fantome.League/IO/RMAN/RMANFile.cs
@@ -30,6 +30,7 @@ namespace Fantome.Libraries.League.IO.RMAN
                 uint uncompressedFileSize = br.ReadUInt32();
 
                 byte[] uncompressedFile = Compression.DecompressZStandard(br.ReadBytes((int)compressedFileSize));
+                byte[] unknownData = br.ReadBytes(256);
             }
         }
     }
