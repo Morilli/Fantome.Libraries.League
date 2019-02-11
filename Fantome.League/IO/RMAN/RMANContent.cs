a28e4d345050ef4dcbaf3ceb76554ad8f17f2a9a
Crauzer <filip.quitko@gmail.com>
2019-02-11 15:32:17 +0100

    corrected section reading

--- Fantome.League/IO/RMAN/RMANContent.cs
+++ Fantome.League/IO/RMAN/RMANContent.cs
@@ -14,21 +14,20 @@ namespace Fantome.Libraries.League.IO.RMAN
             using (BinaryReader br = new BinaryReader(new MemoryStream(data)))
             {
                 uint section1Length = br.ReadUInt32();
-                ushort[] section1UnknownShorts = new ushort[(section1Length - 4) / 2];
+                ushort[] section1UnknownShorts = new ushort[section1Length];
 
-                for (int i = 0; i < (section1Length - 4) / 2; i++)
+                for (int i = 0; i < section1Length / 2; i++)
                 {
                     section1UnknownShorts[i] = br.ReadUInt16();
                 }
 
-                uint section1Unknown = br.ReadUInt32();
-
                 //---------------------------------------------------------------
 
+                long section2Position = br.BaseStream.Position;
                 uint section2Length = br.ReadUInt32();
-                uint[] section2Offsets = new uint[section2Length / 4];
+                uint[] section2Offsets = new uint[(section2Length - 4) / 4];
 
-                for (int i = 0; i < section2Length / 4; i++)
+                for (int i = 0; i < (section2Length - 4) / 4; i++)
                 {
                     section2Offsets[i] = br.ReadUInt32();
                 }
@@ -36,27 +35,24 @@ namespace Fantome.Libraries.League.IO.RMAN
                 //---------------------------------------------------------------
 
                 long section3Position = br.BaseStream.Position;
-                uint section3Length = br.ReadUInt32();
-                uint[] section3Offsets = new uint[section3Length / 4];
+                uint section3OffsetCount = br.ReadUInt32();
+                uint[] section3Offsets = new uint[section3OffsetCount];
 
-                for (int i = 0; i < section3Length / 4; i++)
+                for (int i = 0; i < section3OffsetCount; i++)
                 {
                     section3Offsets[i] = br.ReadUInt32();
                 }
 
                 //---------------------------------------------------------------
 
+                int section4Unknown1 = br.ReadInt32();
                 uint section4Length = br.ReadUInt32();
-                byte[] section4Unknown1 = br.ReadBytes((int)section4Length - 4);
-                uint section4Unknown2 = br.ReadUInt32();
-
-                //---------------------------------------------------------------
-
+                byte[] section4Unknown2 = br.ReadBytes((int)section4Length - 4);
+                uint section5OffsetsCount = br.ReadUInt32();
                 long section5Position = br.BaseStream.Position;
-                uint section5Length = br.ReadUInt32();
-                uint[] section5Offsets = new uint[(section5Length - 4) / 4];
+                uint[] section5Offsets = new uint[section5OffsetsCount];
 
-                for (int i = 0; i < (section5Length - 4) / 4; i++)
+                for (int i = 0; i < section5OffsetsCount; i++)
                 {
                     section5Offsets[i] = br.ReadUInt32();
                 }
